8d616f9eb4d12a102085eb17cfda163359f005f4
Joseph Kirkish <kirkish.2@gmail.com>
2022-04-05 14:01:07 -0400

    Add files via upload

---
+++ UserService.java
@@ -0,0 +1,42 @@
+package com.coderscampus.Assignment3;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class UserService {
+
+	public User isValid(String username, String password) {
+		for (User user : ThirdAssignment.users) {
+			if (user.getUsername().equals(username) &&
+					user.getPassword().equals(password)) {
+				return user;
+			}
+		}
+		return null;
+	}
+	
+
+	public void giveArrayContents() throws IOException, FileNotFoundException {
+
+		File file = new File("C:\\Users\\kirki\\eclipse-workspace\\Assignment3\\data.txt");
+
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(file));
+			String line = null;
+			int i = 0;
+			while ((line = br.readLine()) != null) {
+				ThirdAssignment.users[i] = new User(line.split(","));
+				System.out.println(ThirdAssignment.users[i]);
+				i++;
+			}
+		} finally {
+			System.out.println("User Array Populated");
+			if (br != null)
+				br.close();
+		}
+	}
+}
